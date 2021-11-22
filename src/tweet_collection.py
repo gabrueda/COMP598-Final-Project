7d09eaa1945fb8484e4944ee9302f4a40ce400d4
Nathalie Redick <>
2021-11-22 17:22:23 -0500

    added gitignore, started tweet collection file

---
+++ src/tweet_collection.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+__authors__ = ['Nathalie Redick', 'Gabriela Rueda', 'Yusen Tang']
+__emails__ = ['nathalie.redick@mail.mcgill.ca', 'gabriela.rueda@mail.mcgill.ca', 'yusen.tang@mail.mcgill.ca']
+__date__ = '22 November 2021'
+
+'''
+- COVID in Canadian social media => filter by location
+- collect 1,000 tweets within a 3 day window
+- all 1,000 posts mention either COVID, vaccination, or a name-brand COVID vaccine AND all are in English
+    - you can choose the exact words, as long as they are related to the context that we mentioned before: vaccine hesitancy
+- each post in your collection should be unique
+'''
+
+#imports 
+import argparse
+import pandas as pd
+
+def main():
+    
+    pass
+    
+    
+if __name__ == 'main':
+    main()
