a50e7e11afc822077aa51a4a485edfa89c1d01af
gabrueda <gabriela.rueda@mail.mcgill.ca>
2021-11-27 17:36:37 -0500

    add location to csv file

--- src/json_to_csv.py
+++ src/json_to_csv.py
@@ -26,7 +26,7 @@ def load_json(input_file):
     return pandas.DataFrame(data)
 
 def write_to_csv(data, output_file):
-    columns_names = ['id', 'url', 'text', 'topic', 'sentiment', 'created_at', 'retweet']
+    columns_names = ['id', 'url', 'text', 'topic', 'sentiment', 'location', 'created_at', 'retweet']
     data = data.reindex(columns=columns_names)
     data.to_csv(output_file, index=False)
     
